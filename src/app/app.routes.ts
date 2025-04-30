170eeb435f40f6117a5374393109b1f3d26939eb
SilvioClaudioJR <silvioclaudiojr10@gmail.com>
2025-04-30 16:14:56 -0300

    config. iniciais

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './folders/login/login.component';
+import { HomeComponent } from './folders/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path: 'home',
+        component: HomeComponent
+    },
+    {
+        path:'',
+        redirectTo: '/login',
+        pathMatch:'full'
+    },
+    {
+        path: '**',
+        redirectTo: '/login',
+    }
+];
---
+++ src/app/folders/login/login.component.ts
@@ -0,0 +1,16 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  imports: [CommonModule, FormsModule, RouterLink],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  name: string = '';
+
+  mostrarTexto: boolean = false;
+}
