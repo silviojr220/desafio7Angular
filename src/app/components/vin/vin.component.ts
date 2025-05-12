022057e241fe272019a73d3f2cd1479d6d0ffa0c
SilvioClaudioJr <silvioclaudiojr10@gmail.com>
2025-05-12 14:48:43 -0300

    atualizar vin

--- src/app/components/vin/vin.component.ts
+++ src/app/components/vin/vin.component.ts
@@ -23,18 +23,20 @@ export class VinComponent {
     if (!vin) return;
 
     console.log('pesquisar disparado, VIN =', vin);
-    this.buscarDadosService.buscarDadosVeiculos(vin).subscribe({
-      next: arr => {
-        console.log('A resposta da API (array):', arr);
-        this.dadosVeiculosData = arr;
+    if (vin.length > 19) {
+      this.buscarDadosService.buscarDadosVeiculos(vin).subscribe({
+        next: arr => {
+          console.log('A resposta da API (array):', arr);
+          this.dadosVeiculosData = arr;
 
-        this.veiculosDetalhado = arr.length > 0 ? arr[0]: undefined;
-      },
-      error: err => {
-        console.error('Erro no subscribe', err);
-        this.dadosVeiculosData = [];
-        this.veiculosDetalhado = undefined;
-      }
-    })
+          this.veiculosDetalhado = arr.length > 0 ? arr[0] : undefined;
+        },
+        error: err => {
+          console.error('Erro no subscribe', err);
+          this.dadosVeiculosData = [];
+          this.veiculosDetalhado = undefined;
+        }
+      })
+    }
   }
 }
